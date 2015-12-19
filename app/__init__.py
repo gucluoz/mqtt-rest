bc4d71875bbe30f0e2118a097f6c1edba73ccdf8
Guclu Ozturk <gucluozturk@gmail.com>
2015-12-19 19:40:22 +0200

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
+
+db = SQLAlchemy()
+
+def create_app(config_name):
+  app = Flask(__name__)
+  app.config.from_object(config[config_name])
+  config[config_name].init_app(app)
+
+  db.init_app(app)
+
+  from .api_v1 import api as api_v1_blueprint
+  app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
+
+  return app
---
+++ app/models.py
@@ -0,0 +1,29 @@
+from . import db
+
+class Temprature(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  m1 = db.Column(db.String)
+  m2 = db.Column(db.String)
+  m3 = db.Column(db.String)
+  m4 = db.Column(db.String)
+  timestamp = db.Column(db.DateTime)
+  value = db.Column(db.Float)
+
+  def serialize(self):
+    return {
+      'm1': self.m1,
+      'm2': self.m2,
+      'm3': self.m3,
+      'm4': self.m4,
+      'value': self.value,
+      'timestamp': self.timestamp
+    }
+
+  def __repr__(self):
+    return '<Temprature {}/{}/{}/{}> -> {}'.format(self.m1, self.m2, self.m3, self.m4, self.value)
+
+  def __init__(self,m1,m2,m3,m4):
+    self.m1 = m1
+    self.m2 = m2
+    self.m3 = m3
+    self.m4 = m4
