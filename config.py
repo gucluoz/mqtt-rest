ad2d17607fd6385ad675841e5226e6a80a134768
Guclu Ozturk <gucluozturk@gmail.com>
2016-01-19 11:06:59 +0200

    Change dev db to mysql since sqlite doesn't allow drop cloumn syntax

--- config.py
+++ config.py
@@ -14,8 +14,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://mqtt:mqtt@localhost/mqtt_rest'
 
 
 class TestingConfig(Config):
